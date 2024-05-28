33ac60ece0259a49f4ad5448267e10daf3712fc3
Aakaive <wonseok031@gmail.com>
2024-05-28 17:34:11 +0900

    테일윈드 적용/인라인 스타일 지정

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,19 @@ import Image from "next/image";
 import Top from '@/components/Top';
 
 export default function main() {
+  const repeatText = () => {
+    const elements = [];
+    for (let i = 0; i < 50; i++) {
+      elements.push(<h1 key={i}>hello-world!</h1>);
+    }
+    return elements;
+  };
   return (
-    <div>
+    <div className="relative">
       <Top />
-      <h1>hello-world!</h1>
+      <div className="h-full">
+        {repeatText()}
+      </div>
     </div>
   );
 }
--- components/Top.tsx
+++ components/Top.tsx
@@ -1,17 +1,21 @@
 import Link from 'next/link';
-import styles from '@/styles/Top.module.css';
+import React from 'react';
+import styles from '@/styles/components/Top.module.css';
 
 const Top = () => {
     return (
-        <div className={styles.top}>
-            <div className={styles.top_innerWrapper}>
-                <div className={styles.nav_left}>
+        <div className="
+            sticky bg-white top-0 bg-opacity-75 shadow-sm z-50
+            backdrop-blur-xl backdrop-saturate-150 backdrop-brightness-100
+        ">
+            <div className="px-4 w-full justify-between relative flex" style={{ maxWidth: '1280px', minHeight: '48px'}}>
+                <div className="items-center flex flex-row mr-1 w-full">
                     <div className={styles.site_logo}>
                         <div>Disquiet*</div>
                     </div>
                 </div>
-                <div className={styles.site_menu}>
-                    <div>로그인</div>
+                <div className="items-center justify-center flex flex-row">
+                    <div className="align-middle text-center whitespace-nowrap">로그인</div>
                 </div>
             </div>
         </div>
