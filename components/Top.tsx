30e63a8aada5765d61905824608ca03297393af9
Aakaive <wonseok031@gmail.com>
2024-05-30 18:02:17 +0900

    탑 컴포넌트 요소 작성

--- components/Top.tsx
+++ components/Top.tsx
@@ -9,13 +9,31 @@ const Top = () => {
             backdrop-blur-xl backdrop-saturate-150 backdrop-brightness-100
         ">
             <div className="px-4 w-full justify-between relative flex" style={{ maxWidth: '1280px', minHeight: '48px'}}>
+                {/* 탑_이너래퍼 */}
                 <div className="items-center flex flex-row mr-1 w-full">
+                    {/* 탑_좌측 */}
                     <div className={styles.site_logo}>
+                        {/* 사이트 로고 */}
                         <div>Disquiet*</div>
+                    </div>
+                    <div className="">
+                        {/* 검색창 */}
                     </div>
                 </div>
                 <div className="items-center justify-center flex flex-row">
-                    <div className="align-middle text-center whitespace-nowrap">로그인</div>
+                    {/* 탑_우측 */}
+                    <div>
+                        {/* 내비게이션 리스트 */}
+                        <div>
+                            {/* 사이트 소개 세부 내비게이션 리스트 */}
+                        </div>
+                    </div>
+                    <div>
+                        {/* 세퍼레이터 */}
+                    </div>
+                    <div>
+                        {/* 로그인/회원가입 버튼 */}
+                    </div>
                 </div>
             </div>
         </div>
