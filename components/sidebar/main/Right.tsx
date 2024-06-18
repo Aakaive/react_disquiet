3d9c2214ec6d3aee9d446574952593bcd58398b2
Aakaive <wonseok031@gmail.com>
2024-06-18 16:00:09 +0900

    푸터 완성

--- components/sidebar/main/Right.tsx
+++ components/sidebar/main/Right.tsx
@@ -15,12 +15,18 @@ const Right = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000); // 2초마다 이미지 전환
+        const intervalId = setInterval(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 3000); // 2초마다 이미지 전환
 
-    return () => clearInterval(intervalId); // 컴포넌트 언마운트 시 인터벌 클리어
-  }, []);
+        return () => clearInterval(intervalId); // 컴포넌트 언마운트 시 인터벌 클리어
+    }, []);
+
+    const [isToggled, setIsToggled] = useState(false);
+
+    const handleToggle = () => {
+        setIsToggled(!isToggled);
+    };
 
     return (
         <div className="mt-[32px] min-w-[246px]">
@@ -218,6 +224,18 @@ const Right = () => {
                         </div>
                         <div className="flex flex-col ml-[-4px] text-[12px] leading-[18px] text-[rgb(142,142,142)] gap-[4px]">
                             {/* 사업자 정보 */}
+                            <div className="items-center flex leading-[13.2px] cursor-pointer" onClick={handleToggle}>
+                                <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" 
+                                        className={`${isToggled ? "rotate-90" : "rotate-0"} align-middle transition-all duration-100 ease-out fill-[rgb(196,196,196)]`}
+                                >
+                                    <path d="M14.959 12.0393C15.3281 11.8284 15.3193 11.2922 14.959 11.0813L9.89648 8.09302C9.50977 7.87329 9 8.05786 9 8.48853V14.6233C9 15.054 9.4834 15.2649 9.89648 15.0188L14.959 12.0393Z"></path>
+                                </svg>
+                                디스콰이엇 사업자 정보.
+                            </div>
+                            <div className={`${isToggled ? "opacity-100 visible max-h-fit" : "opacity-0 invisible max-h-0"} items-center my-[2px] ml-2 mr-1 pl-[15px] overflow-hidden leading-[19.2px] transition-opacity duration-500 ease-in-out`}>
+                                주식회사 디스콰이엇 / 대표자: 박현솔 / 사업자등록번호: 274-88-02252 / 통신판매업신고: 2023-서울강남-01585 / 서울특별시 강남구 역삼로 180 2층
+                                <div className="mt-[8px]">© 2021 Disquiet</div>
+                            </div>
                         </div>
                     </div>
                 </div>
